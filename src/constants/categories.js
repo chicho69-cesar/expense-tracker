fb1654a6b84ba7ee8c4f35b69dabe89cd18b5c75
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-04 18:24:52 -0600

    :sparkles: Add category constants

---
+++ src/constants/categories.js
@@ -0,0 +1,10 @@
+export const CATEGORIES = [
+  { id: 'accounts-and-payments', text: 'Cuentas y Pagos' },
+  { id: 'clothes', text: 'Ropa' },
+  { id: 'food', text: 'Comida' },
+  { id: 'fun', text: 'Diversion' },
+  { id: 'health-and-hygiene', text: 'Salud e Higiene' },
+  { id: 'home', text: 'Hogar' },
+  { id: 'shopping', text: 'Compras' },
+  { id: 'transport', text: 'Transporte' }
+]
