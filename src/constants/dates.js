daf302c72f3dec4972eb3007847c86e46decc6ce
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-04 18:25:39 -0600

    :sparkles: Add dates constants for the project

---
+++ src/constants/dates.js
@@ -0,0 +1,2 @@
+export const MONTHS = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+export const SHORT_WEEK_DAYS = ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa']
